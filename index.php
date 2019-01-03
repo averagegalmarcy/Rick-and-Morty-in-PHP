ca66c7d4ddf2618f8d12bac6d74c89c13193a353
Marcy Silverman <marcysilve225@gmail.com>
2019-01-02 21:44:09 -0800

    set up the page

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="styles.css">
+  <title>PHP SITE ABOUT RICK AND MORTY</title>
+</head>
+<body>
+  <header>
+    <h1>This is the header</h1>
+    <nav>
+      <ul>
+        <li><a href="index.php">About</a></li>
+        <li><a href="index.php?about">Characters</a></li>
+        <li><a href="index.php?blog">Blog</a></li>
+        <li><a href="index.php?contact">Contact</a></li>
+      </ul>
+    </nav>
+   </header>
+  <div id="content">
+    <h3>Content</h2>
+  </div>
+  
+</body>
+</html>
